ca5413a829d6855dc532fe5754bc8f3afaf9ff14
Aaron Klotnia <aklotnia@gmail.com>
2019-12-08 16:17:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(ar, event) {
+    let final_ar = []
+    for (let i = 0; i < ar.length; i++) {
+        final_ar.push(`Thank you, ${ar[i]}, for the wonderful ${event} gift!`)
+    }
+    return final_ar
+}
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
