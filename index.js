a3d289ac0fb5d2ed37f27b2f07e1e1821741e4b5
hdp2112 <hdp2112@columbia.edu>
2019-12-08 17:35:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(array, surprise) {
+    let newarray = [];
+    for (let i = 0; i < array.length ; i++) {
+        newarray.push(`Thank you, ${array[i]}, for the wonderful ${surprise} gift!`);
+    }
+    return newarray;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num -= 1;
+    }
+}
