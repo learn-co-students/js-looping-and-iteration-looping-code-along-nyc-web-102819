3d143a8c4cb5e5aa0c88346067f3ca18d6907a00
MarioBland <gabedotgutierrez@gmail.com>
2019-12-08 18:06:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+
+function writeCards(names, event){
+    let solution = [];
+    for (let i = 0; i < names.length; i++){
+        solution.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return solution
+}
+
+// function writeCards( namesArray, event ) {
+//   let thankYouCards = []
+//   for ( let i = 0; i < namesArray.length; i++ ) {
+//     thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+//   }
+//   return thankYouCards
+// }
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number)
+        number --
+    }
+}
