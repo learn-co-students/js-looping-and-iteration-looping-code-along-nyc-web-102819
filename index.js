605841c8f0dacba13dc6d85a13f1a7f345c5342a
Pavel Beletsky <pavel.beletsky@live.com>
2019-12-09 03:22:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function writeCards(ar, name) {
+  const newAr = []
+  for (let i = 0; i < ar.length; i++) {
+    let element = `Thank you, ${ar[i]}, for the wonderful ${name} gift!`;
+    newAr.push(element)
+  }
+  return newAr
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num--);
+  }  
+}
