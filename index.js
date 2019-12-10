5986488d99523da900276f30bb4b0dae37fca01a
hasibulc <hasibulc91@gmail.com>
2019-12-09 20:49:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(nameArray, typeOfGift) {
+    let array = []
+    for (let i = 0; i < nameArray.length; i++) {
+        array.push(`Thank you, ${nameArray[i]}, for the wonderful ${typeOfGift} gift!`);
+    }
+    return array;
+}
+
+function countDown(number) {
+    let i = 0;
+    while (number >= i) {
+        console.log(number);
+        number--;
+    }
+}
